130e40582e5185d778ad72cc49e6516b7c8bf997
Steve Hillier <stephenhillier@gmail.com>
2019-10-27 18:19:15 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='starlette_exporter',
@@ -6,10 +6,11 @@ setup(
     author='Stephen Hillier',
     author_email='stephenhillier@gmail.com',
     packages=['starlette_exporter'],
-    license='LICENSE',
+    license='Apache License 2.0',
     url="https://github.com/stephenhillier/starlette_exporter",
     description='Prometheus metrics exporter for Starlette applications.',
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     install_requires=[
         "prometheus_client",
         "starlette"
