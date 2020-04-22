5fe2f9a9c073f6c005e4a3fdebe1989f2d8ad51e
Steve Hillier <stephenhillier@gmail.com>
2020-04-21 20:56:56 -0700

    increase version to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='starlette_exporter',
-    version='0.1.0',
+    version='0.3.0',
     author='Stephen Hillier',
     author_email='stephenhillier@gmail.com',
     packages=['starlette_exporter'],
