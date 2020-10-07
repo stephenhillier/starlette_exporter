add768e20b27540b04397e621c394f2a1703a691
Robert Coup <robert@coup.net.nz>
2020-10-07 10:31:48 +0100

    Change middleware to be a generic ASGI3 middleware.
    Starlette is dropping BaseHTTPMiddleware because it causes all sorts of problems with streaming requests & responses.

--- starlette_exporter/middleware.py
+++ starlette_exporter/middleware.py
@@ -1,8 +1,8 @@
 """ Middleware for exporting Prometheus metrics using Starlette """
 import time
 from prometheus_client import Counter, Histogram
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.types import ASGIApp
+from starlette.requests import Request
+from starlette.types import ASGIApp, Message, Receive, Scope, Send
 from logging import getLogger
 
 logger = getLogger("exporter")
@@ -22,16 +22,22 @@ REQUEST_COUNT = Counter(
 )
 
 
-class PrometheusMiddleware(BaseHTTPMiddleware):
+class PrometheusMiddleware:
     """ Middleware that collects Prometheus metrics for each request.
         Use in conjuction with the Prometheus exporter endpoint handler.
     """
     def __init__(self, app: ASGIApp, group_paths: bool = False, app_name: str = "starlette"):
-        super().__init__(app)
+        self.app = app
         self.group_paths = group_paths
         self.app_name = app_name
 
-    async def dispatch(self, request, call_next):
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        if scope["type"] not in ["http"]:
+            await self.app(scope, receive, send)
+            return
+
+        request = Request(scope)
+
         method = request.method
         path = request.url.path
         begin = time.time()
@@ -40,28 +46,29 @@ class PrometheusMiddleware(BaseHTTPMiddleware):
         # or an unhandled exception occurs.
         status_code = 500
 
+        async def wrapped_send(message: Message) -> None:
+            if message['type'] == 'http.response.start':
+                nonlocal status_code
+                status_code = message['status']
+
+            await send(message)
+
         try:
-            response = await call_next(request)
-            status_code = response.status_code
-
-        except Exception as e:
-            raise e
-
+            await self.app(scope, receive, wrapped_send)
         finally:
             # group_paths enables returning the original router path (with url param names)
             # for example, when using this option, requests to /api/product/1 and /api/product/3
             # will both be grouped under /api/product/{product_id}. See the README for more info.
             if self.group_paths and request.scope.get('endpoint', None) and request.scope.get('router', None):
-
                 try:
                     # try to match the request scope's handler function against one of handlers in the app's router.
                     # if a match is found, return the path used to mount the handler (e.g. api/product/{product_id}).
                     path = [
                         route for route in request.scope['router'].routes
-                            if (hasattr(route, 'endpoint') and route.endpoint  == request.scope['endpoint'])
-                            # for endpoints handled by another app, like fastapi.staticfiles.StaticFiles,
-                            # check if the request endpoint matches a mounted app.
-                            or (hasattr(route, 'app') and route.app == request.scope['endpoint']) 
+                        if (hasattr(route, 'endpoint') and route.endpoint == request.scope['endpoint'])
+                        # for endpoints handled by another app, like fastapi.staticfiles.StaticFiles,
+                        # check if the request endpoint matches a mounted app.
+                        or (hasattr(route, 'app') and route.app == request.scope['endpoint'])
                     ][0].path
                 except IndexError:
                     # no route matched.
@@ -76,5 +83,3 @@ class PrometheusMiddleware(BaseHTTPMiddleware):
 
             REQUEST_COUNT.labels(*labels).inc()
             REQUEST_TIME.labels(*labels).observe(end - begin)
-
-        return response
