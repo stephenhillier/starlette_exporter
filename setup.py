451489b9cf1324cb0130f74713c14c4a74020551
Steve Hillier <stephenhillier@gmail.com>
2021-05-10 20:21:33 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='starlette_exporter',
-    version='0.7.0',
+    version='0.8.0',
     author='Stephen Hillier',
     author_email='stephenhillier@gmail.com',
     packages=['starlette_exporter'],
