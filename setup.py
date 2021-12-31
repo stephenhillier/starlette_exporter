269b1f631749e0599efb4c0f4b9adc70151f211f
Steve Hillier <stephenhillier@gmail.com>
2021-12-31 14:12:55 -0800

    v0.12.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='starlette_exporter',
-    version='0.11.0',
+    version='0.12.0',
     author='Stephen Hillier',
     author_email='stephenhillier@gmail.com',
     packages=['starlette_exporter'],
