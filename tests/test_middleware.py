e85add5f99a829344c757df9bba1e0b258495881
Ionut Popovici <ipvlocked@gmail.com>
2022-02-15 08:45:20 +0000

    optional metrics test 0.3 space fix

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -467,5 +467,5 @@ class TestOptionalMetrics:
         metrics = client.get('/metrics').content.decode()
         response_size_metric = [s for s in metrics.split('\n') if (
             'starlette_requests_response_body_size_total' in s and 'path="/200"' in s)]
-        response_size = response_size_metric[0].split('}')[1]
+        response_size = response_size_metric[0].split('} ')[1]
         assert response_size == 0.0
