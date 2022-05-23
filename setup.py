d6107220ff7f430c8544d61256b28220cab6fd8e
Steve Hillier <stephenhillier@gmail.com>
2022-05-22 21:21:50 -0700

    v0.13.0

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='starlette_exporter',
-    version='0.12.0',
-    author='Stephen Hillier',
-    author_email='stephenhillier@gmail.com',
-    packages=['starlette_exporter'],
-    license='Apache License 2.0',
+    name="starlette_exporter",
+    version="0.13.0",
+    author="Stephen Hillier",
+    author_email="stephenhillier@gmail.com",
+    packages=["starlette_exporter"],
+    license="Apache License 2.0",
     url="https://github.com/stephenhillier/starlette_exporter",
-    description='Prometheus metrics exporter for Starlette applications.',
-    long_description=open('README.md').read(),
+    description="Prometheus metrics exporter for Starlette applications.",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    install_requires=[
-        "prometheus_client",
-        "starlette"
-    ],
+    install_requires=["prometheus_client", "starlette"],
 )
